7f34ce21411baacab92c86dd129a73b6570cc8ad
Brian Alsup <Brian@BetamoreAdmins-MacBook-Air-5.local>
2022-02-02 18:34:59 -0500

    added code to parse bitcoin csv file

--- crypto.js
+++ crypto.js
@@ -1,3 +1,14 @@
+const csv = require('csv-parser');
+const fs = require('fs');
 
+let a = []
+fs.createReadStream('coin_Bitcoin.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    a.push(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
 
 
